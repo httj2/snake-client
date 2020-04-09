93347023dc45b4fcb809d52f1734de99ed7be196
Judith Huynh <judithuynh@gmail.com>
2020-04-09 11:16:41 +0000

    updated to ES6, and removed host IP and Port hard code to constants.js

--- client.js
+++ client.js
@@ -1,25 +1,31 @@
-
 const net = require('net');
+// const setupInput = require('./play')
+const { IP, PORT } = require('./constants');
+
 
 // * Establishes connection with the game server
 // */
-const connect = function() {
- const conn = net.createConnection({ 
-   host: '10.0.2.15',
-   port: 50541
- });
- // interpret incoming data as text
- conn.setEncoding('utf8'); 
- // allows us to recieve data from the server
- conn.on('data', (data) => {
-   console.log('Server: ', data);
- });
-//  "connect" event is triggered on a connection as soon as it is successfully established.
- conn.on('connect', () => {
-  console.log("we are connected!" )
-  conn.write("Name: JHJ");
-});
- return conn;
-}
+const connect = () => {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  // allows us to recieve data from the server
+  conn.on('data', (data) => {
+    console.log('Server: ', data);
+  });
+  //  "connect" event is triggered on a connection as soon as it is successfully established.
+  conn.on('connect', () => {
+    console.log("we are connected!");
+    conn.write("Name: JHJ");
+  });
+  conn.on('connect', () => {
+    conn.write("Say: Do you like pizza?");
+  });
+  return conn;
+};
+
 
 module.exports = { connect };
