52ffba0d050ce2603e4fbc60707592511d857547
Judith Huynh <judithuynh@gmail.com>
2020-04-09 00:38:23 +0000

    added an event listener that allows keys to move the snake; use connection variabel to send movements

--- input.js
+++ input.js
@@ -1,19 +1,38 @@
-
 const stdin = process.stdin;
-const setupInput = () => {
+let connection; 
+
+const setupInput = (conn) => {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   handleUserInput();
   return stdin;
  }
+ 
 const handleUserInput = () => {
   stdin.on('data', (key) => {
      if (key === '\u0003') {
-      process.exit();
-     }
-   });
-   return stdin;
+      process.exit(); 
+    }
+  stdin.on('data', (key) => {
+    if (key === 'w') {
+      // console.log('up')
+      connection.write("Move: up")
+    }
+    if (key === 'a') {
+      connection.write("Move: left")
+    }
+    if (key === 's') {
+      connection.write("Move: down")
+    }
+    if (key === 'd') {
+      connection.write("Move: right")
+    }
+  });
+  });
+  return stdin;
+ 
  }
  
 
